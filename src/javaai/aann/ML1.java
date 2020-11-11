d252a80f1be997d74ee5600f71d49ca63be85cff
Abuchi Obiegbu <aobiegb@us.ibm.com>
2020-11-10 23:09:00 -0500

    added project code

--- src/javaai/aann/ML1.java
+++ src/javaai/aann/ML1.java
@@ -1,27 +1,54 @@
+/*
+ * Author:               Abuchi Obiegbu
+ * Assignment:           Lab 1
+ * Class:                Artificial Intelligence
+ * Copyright (c) 2020 Abuchi Obiegbu
+ */
 package javaai.aann;
 
 import java.util.*;
 
-public class ML1 extends BaseML {
+/**
+ * This class reads the iris csv file, populates a hashmap for
+ * classification purposes and tests classification.
+ * @author Abuchi Obiegbu
+ * @version 1.0
+ */
+public class ML1 extends BaseML{
+    /**
+     * Loads data and populate a hashmap for classification purposes
+     * @param target species hashmap
+     */
+    public static void load(Map<Measure, Species> target) {
+        // Load iris data from csv
+        BaseML.load();
+
+        // Store data in a hashmap for classification purposes
+        for(int k=0; k < measures.size(); k++) {
+            target.put(measures.get(k), flowers.get(k));
+        }
+    }
+
     public static void main(String[] args) {
-        Map<Measure, Species> map = new HashMap<>();
+        // species hashmap
+        Map<Measure, Species> map = new HashMap<>();    // HashMap is a Map -> Polymorphism
+
+        // populate species hashmap
         load(map);
 
-        List<Measure> tests = new ArrayList<>(Arrays.asList(new Measure(5.1,3.5,1.4,0.2)));
+        // list of test cases
+        List<Measure> tests =
+                new ArrayList<>(Arrays.asList(
+                        measures.get(93),   // get the measure found at index 93 instead of using the string
+                        measures.get(1),
+                        measures.get(2),
+                        measures.get(3),
+                        measures.get(4)));
 
+        // testing classification
         for(Measure measure: tests) {
             System.out.println(map.get(measure) + " " + measure);
         }
-
-        Measure m1 = new Measure(5.9,3.0,5.1,1.8);
-        Measure m2 = new Measure(5.9,3.0,5.1,1.8);
-        System.out.println(m1.equals(m2));
-    }
-
-    public static void load(Map<Measure, Species> target) {
-        BaseML.load();
-
-        for(int k = 0; k < measures.size(); k++)
-            target.put(measures.get(k),flowers.get(k));
     }
 }
+
