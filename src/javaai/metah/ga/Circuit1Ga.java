acfe914b64059308c044f42cc9c7f16a70805373
Abuchi Obiegbu <aobiegb@us.ibm.com>
2020-11-19 21:47:49 -0500

    added phase3 assignment

---
+++ src/javaai/metah/ga/Circuit1Ga.java
@@ -0,0 +1,261 @@
+/*
+ * Author:               Abuchi Obiegbu
+ * Assignment:           Project Phase 3
+ * Class:                Artificial Intelligence
+ * Copyright (c) 2020 Abuchi Obiegbu
+ */
+package javaai.metah.ga;
+
+import org.encog.ml.ea.genome.Genome;
+import org.encog.ml.ea.population.BasicPopulation;
+import org.encog.ml.ea.population.Population;
+import org.encog.ml.ea.species.BasicSpecies;
+import org.encog.ml.ea.species.Species;
+import org.encog.ml.ea.train.basic.TrainEA;
+import org.encog.ml.genetic.crossover.Splice;
+import org.encog.ml.genetic.genome.DoubleArrayGenome;
+import org.encog.ml.genetic.genome.DoubleArrayGenomeFactory;
+
+import static javaai.util.Helper.asString;
+
+/**
+ * This class uses a genetic algorithm for unsupervised learning to solve y = (x-3)^2.
+ */
+public class Circuit1Ga {
+
+    /** Stopping criteria as difference between best solution and last best one */
+    public final double TOLERANCE = 0.01;
+
+    /** Convergence criteria: number of times best solution stays best */
+    public final static int MAX_SAME_COUNT = 100;
+
+    /** Population has this many individuals. */
+    public final static int POPULATION_SIZE = 10000;
+
+    /** Chromosome size (ie, number of genes): domain is [0, (2^n-1)]. */
+    public final static int GENOME_SIZE = 10;
+
+    /** Mutation rate */
+    public final static double MUTATION_RATE = 0.01;
+
+    /** Same count for test of convergence */
+    protected int sameCount = 0;
+
+    /** Last y value of training iteration */
+    protected double yLast;
+
+    double fitness = 0.0;
+
+    /**
+     * Runs the program.
+     * @param args Command line arguments not used.
+     */
+    public static void main(String[] args) {
+        Circuit1Ga ga = new Circuit1Ga();
+
+        DoubleArrayGenome best = ga.solve();
+
+        System.out.println("best ="+asString(best) + " fitness = " + ga.fitness);
+    }
+
+    /**
+     * Solves the objective.
+     * @return Best individual
+     */
+    public DoubleArrayGenome solve() {
+        // Initialize a population
+        Population population = initPop();
+        // output("before", population);
+
+        // instance of Circuit1Objective
+        Circuit1Objective objective = new Circuit1Objective();
+
+        // Create the evolutionary training algorithm
+        TrainEA ga = new TrainEA(population, objective);
+
+        // Set the mutation rate: 2nd operation tends to give better results.
+        // ga.addOperation(MUTATION_RATE, new MutateShuffle());
+        ga.addOperation(MUTATION_RATE, new MutateDoubleArrayGenome(0.001));
+
+        // Set up to splice along the middle of the genome
+        ga.addOperation(0.9, new Splice(GENOME_SIZE /2));
+
+        // Do the learning algorithm
+        train(ga);
+
+        // Return the best individual
+        DoubleArrayGenome best = (DoubleArrayGenome)ga.getBestGenome();
+
+        ga.getPopulation();
+
+        // output("after", population);
+
+        // get the fitness value
+        fitness = getFitness(best.getData(), objective);
+
+
+        return best;
+    }
+
+    /**
+     * Runs the learning algorithm.
+     * @param ga
+     */
+    protected void train(TrainEA ga) {
+        int iteration = 0;
+
+        boolean converged = false;
+
+        // display header
+        System.out.printf("%3s %8s %5s %5s", "#", "fitness", "same", "best");
+
+        System.out.println();
+
+        // Loop until the best answer doesn't change for a while
+        while(!converged) {
+
+            ga.iteration();
+
+            // Get the value of the best solution for predict(x)
+            double y = ga.getError();
+
+            DoubleArrayGenome best = (DoubleArrayGenome) ga.getBestGenome();
+
+            System.out.printf("%3d %7.4f %4d %82s\n",iteration, y, sameCount, asString(best));
+
+            iteration++;
+
+            converged = didConverge(y, ga.getPopulation());
+        }
+    }
+
+    /**
+     * Tests whether GA has converged.
+     * @param y Y value in y=predict(x)
+     * @param pop Population of individuals
+     * @return True if the GA has converge, otherwise false
+     */
+    public boolean didConverge(double y, Population pop) {
+        if(sameCount >= MAX_SAME_COUNT)
+            return true;
+
+        if(Math.abs(yLast - y) < TOLERANCE) {
+            sameCount++;
+        }
+        else
+            sameCount = 0;
+
+        yLast = y;
+
+        return false;
+    }
+
+    /**
+     * Initializes a population.
+     * @return Population
+     */
+    protected Population initPop() {
+        Population pop = new BasicPopulation(POPULATION_SIZE, null);
+
+        BasicSpecies species = new BasicSpecies();
+
+        species.setPopulation(pop);
+
+        for(int k=0; k < POPULATION_SIZE; k++) {
+            final DoubleArrayGenome genome = randomGenome(GENOME_SIZE);
+
+            species.getMembers().add(genome);
+        }
+
+        pop.setGenomeFactory(new DoubleArrayGenomeFactory(GENOME_SIZE));
+        pop.getSpecies().add(species);
+
+        return pop;
+    }
+
+    /**
+     * Gets a random individual
+     * @param sz Number of genes
+     * @return
+     */
+    public DoubleArrayGenome randomGenome(int sz) {
+        DoubleArrayGenome genome = new DoubleArrayGenome(sz);
+
+        final double[] organism = genome.getData();
+
+        for(int k=0; k < organism.length; k++) {
+            // get a random weight
+            organism[k] = Circuit1Objective.getRandomWeight();
+        }
+
+        return genome;
+    }
+
+    /**
+     * Dumps the population.
+     * @param title Title
+     * @param pop Population
+     */
+    protected void output(final String title, final Population pop) {
+        final Species species = pop.getSpecies().get(0);
+
+        System.out.println("----- "+title);
+
+        int n = 1;
+
+        for (Genome genome : species.getMembers()) {
+            DoubleArrayGenome individual = (DoubleArrayGenome) genome;
+
+            System.out.printf("%5d %s\n",n, asString(individual));
+
+            n++;
+        }
+    }
+
+    /**
+     * Calculates the fitness of interneuron weights using the root mean square error
+     * @param ws interneuron weights
+     * @param objective instance of circuit1 objective
+     * @return Fitness value
+     */
+    public double getFitness(double[] ws, Circuit1Objective objective) {
+        // Sum of square error
+        double sumSqrErr = 0.0;
+
+        // This is the encoded column header
+        String reportHeader = String.format("%3s %4s %4s %4s %4s", "x1", "x2", "x3", "t1", "y1");
+
+        System.out.println(reportHeader);
+
+
+        // loop through all the XOR_INPUTS
+        for(int k=0; k < objective.XOR_INPUTS.length; k++) {
+            double x1 = objective.XOR_INPUTS[k][0];
+            double x2 = objective.XOR_INPUTS[k][1];
+            double x3 = objective.XOR_INPUTS[k][2];
+
+            // calculate actual output
+            double y1 = objective.feedforward(x1, x2, x3, ws);
+
+            // ideal output
+            double t1 = objective.XOR_IDEALS[k][0];
+
+            // Square error
+            double delta = (y1 - t1);
+            double sqrError = objective.square(delta);
+
+            // Sum the square error
+            sumSqrErr += sqrError;
+
+            System.out.printf("%1.1f %4.1f %4.1f %4.1f %10.6f", x1, x2, x3, t1, y1);
+            System.out.println();
+        }
+
+        // calculate RMSE
+        double rmse = Math.sqrt(sumSqrErr / objective.XOR_INPUTS.length);
+
+        return rmse;
+    }
+}
+
+
--- src/javaai/metah/ga/Circuit1Objective.java
+++ src/javaai/metah/ga/Circuit1Objective.java
@@ -1,6 +1,6 @@
 /*
  * Author:               Abuchi Obiegbu
- * Assignment:           Project Phase 2
+ * Assignment:           Project Phase 3
  * Class:                Artificial Intelligence
  * Copyright (c) 2020 Abuchi Obiegbu
  */
@@ -8,18 +8,16 @@ package javaai.metah.ga;
 
 import org.encog.ml.CalculateScore;
 import org.encog.ml.MLMethod;
-import org.encog.ml.genetic.genome.IntegerArrayGenome;
+import org.encog.ml.genetic.genome.DoubleArrayGenome;
 
 import java.util.Random;
-
-import static javaai.util.Helper.asInt;
 
 /**
  * This class calculates the fitness of an individual chromosome or phenotype.
  */
 class Circuit1Objective implements CalculateScore {
 
-    public final static boolean DEBUGGING = true;
+    public final static boolean DEBUGGING = false;
     public final static String TEAM = "Abuchi Obiegbu";
     public final static int NUM_WEIGHTS = 10;
     public final static double RANGE_MAX = 10.0;
@@ -70,12 +68,13 @@ class Circuit1Objective implements CalculateScore {
     }
 
     /**
-     * Calculates the sigmoid activation function
-     * @param z value to calculate
+     * Sigmoid activation function
+     * @param z input
      * @return sigmoid value of z
      */
     protected double sigmoid(double z) {
-        return (1/(1 + Math.exp(-1*z)));
+        double y = 1.0 / (1 + Math.exp(-z));
+        return y;
     }
 
     /**
@@ -83,8 +82,8 @@ class Circuit1Objective implements CalculateScore {
      * @param x1 XOR input
      * @param x2 XOR input
      * @param x3 XOR input
-     * @param ws interneuron weights
-     * @return double precision value of y1
+     * @param ws Weights w1-w8 and b1 and b2 -- in this order
+     * @return Actual, that is, Y1
      */
     public double feedforward(double x1, double x2, double x3, double[] ws) {
         // local variables to hold w1-w8, b1, b2 values
@@ -100,9 +99,12 @@ class Circuit1Objective implements CalculateScore {
         double b2 = ws[9];
 
         /* Implements Equations 1, 2 and 3 */
-        double h1 = sigmoid(w1 * x1 + w3 * x2 + b1 * 1 + w7 * x3);
-        double h2 = sigmoid(w2 * x1 + w4 * x2 + b1 * 1 + w8 * x3);
-        double y1 = sigmoid(w5 * h1 + w6 * h2 + b2 * 1);
+        double zh1 = w1 * x1 + w3 * x2 + b1 * 1.0 + w7 * x3;
+        double zh2 = w2 * x1 + w4 * x2 + b1 * 1.0 + w8 * x3;
+        double h1 = sigmoid(zh1);
+        double h2 = sigmoid(zh2);
+        double zy1 = w5 * h1 + w6 * h2 + b2 * 1.0;
+        double y1 = sigmoid(zy1);
 
         return y1;
     }
@@ -110,14 +112,11 @@ class Circuit1Objective implements CalculateScore {
     /**
      * Calculates the fitness of interneuron weights using the root mean square error
      * @param ws interneuron weights
-     * @return double precision root mean square error value
+     * @return Fitness value
      */
     public double getFitness(double[] ws) {
-        double y1 = 0.0;
-        double t1;
-        double squareValue = 0.0;
-        double mean = 0.0;
-        double root = 0.0;
+        // Sum of square error
+        double sumSqrErr = 0.0;
 
         // This is the encoded column header
         String reportHeader = String.format("%2s %4s %7s %7s %7s %7s","#", "x1", "x2", "x3", "t1", "y1");
@@ -128,31 +127,36 @@ class Circuit1Objective implements CalculateScore {
         }
 
         // loop through all the XOR_INPUTS
-        for(int row=0; row < XOR_INPUTS.length; row++) {
-            // cslculate actual output
-            y1 = feedforward(XOR_INPUTS[row][0], XOR_INPUTS[row][1], XOR_INPUTS[row][2], ws);
+        for(int k=0; k < XOR_INPUTS.length; k++) {
+            double x1 = XOR_INPUTS[k][0];
+            double x2 = XOR_INPUTS[k][1];
+            double x3 = XOR_INPUTS[k][2];
+
+            // calculate actual output
+            double y1 = feedforward(x1, x2, x3, ws);
 
             // ideal output
-            t1 = XOR_IDEALS[row][0];
+            double t1 = XOR_IDEALS[k][0];
 
-            // compute square value
-            squareValue += square(y1 - t1);
+            // Square error
+            double delta = (y1 - t1);
+            double sqrError = square(delta);
+
+            // Sum the square error
+            sumSqrErr += sqrError;
 
             // display only when debug is turned on
             if (DEBUGGING) {
                 System.out.printf("%2d %8.4f %7.4f %7.4f %7.4f %7.4f",
-                        row+1, XOR_INPUTS[row][0], XOR_INPUTS[row][1], XOR_INPUTS[row][2], t1, y1);
+                        k+1, x1, x2, x3, t1, y1);
                 System.out.println();
             }
         }
 
-        // calculate mean
-        mean = (squareValue / (double)XOR_INPUTS.length);
+        // calculate RMSE
+        double rmse = Math.sqrt(sumSqrErr / XOR_INPUTS.length);
 
-        // calculate root
-        root = Math.sqrt(mean);
-
-        return root;
+        return rmse;
     }
 
     /**
@@ -171,13 +175,10 @@ class Circuit1Objective implements CalculateScore {
      */
     @Override
     public double calculateScore(MLMethod phenotype) {
-        IntegerArrayGenome genome = (IntegerArrayGenome) phenotype;
+        DoubleArrayGenome genome = (DoubleArrayGenome) phenotype;
 
-        int x = asInt(genome);
-
-        double y = f(x);
-
-        return y;
+        // get the interneuron weights from the genome and returns fitness
+        return getFitness(genome.getData());
     }
 
     /**
